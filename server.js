e88c2a1630af5b90d58643b08d33029464bc76f6
Equate <Equate@Equate-Vijay.local>
2016-06-07 11:27:46 -0700

    List page initial commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express'),
 
 app.use(express.static('public'));
 
+app.post('/list', function(req, res){
+	res.sendFile(path.join(__dirname + '/list.html'));
+})
+
 app.listen(process.env.PORT || 3000, function(){
 	console.log('connected');
 })
